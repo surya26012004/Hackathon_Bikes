1e2d2df205e67b26097fd7f5d8aa9a3c08e078e1
k surya <150570522+surya26012004@users.noreply.github.com>
2026-01-21 12:42:30 +0530

    Revert "testing 2"

--- src/main/java/org/example/base.java
+++ src/main/java/org/example/base.java
@@ -1,11 +1,8 @@
 package org.example;
 
-import org.testng.annotations.Test;
-
 public class base {
     @Test
     public void display() {
-        System.out.println("test");
         System.out.println("This is the base class.");
     }
 
